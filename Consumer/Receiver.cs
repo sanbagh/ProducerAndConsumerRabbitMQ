439c3af7822f4b4e8ce40dcd19c26de967a53fff
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-07-26 15:32:49 +0530

    ProducerConsumerRabbitMQ

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicQueue", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) => {
+                    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+                };
+                channel.BasicConsume("BasicQueue", true, consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicQueue", false, false, false, null);
+                    var message = "This is sender";
+                    channel.BasicPublish("", "BasicQueue",null, Encoding.UTF8.GetBytes(message));
+                    Console.WriteLine("message sent...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
